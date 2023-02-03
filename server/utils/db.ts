3b3976d9722547d2dfec4dd86ab163b00894a2a0
Mayolito Waje <mayolito.waje08@gmail.com>
2023-02-03 11:56:44 +0800

    add logger functions

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import * as config from './config.js';
+import * as logger from './logger.js';
 
 mongoose.set('strictQuery', true);
 
 const connectToMongoDB = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(config.MONGODB_URI as string);
-    console.log('connected to MongoDB: ', conn.connection.host);
+    logger.info('connected to MongoDB: ', conn.connection.host);
   } catch (error) {
-    console.log('Error connecting to MongoDB: ', error);
+    logger.error('Error connecting to MongoDB: ', error);
   }
 };
 
